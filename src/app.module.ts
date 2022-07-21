8a0d6a3b95f1f9d096a14547fb9c0602da3fc916
kiryl.dryha <kiryl.dryha@sap.com>
2022-07-21 19:01:08 +0400

    setup migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
+      port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
